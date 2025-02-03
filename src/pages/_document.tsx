49fa583fc4686b2418558febbb9e6ba394d1a028
Carlos Augustoo <carlos.aug.developer@gmail.com>
2025-02-03 18:34:20 -0300

    style: configured next-themes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,20 @@
+import { ThemeProvider } from "@/context/ThemeProvider";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="h-full">
       <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
+      <body className="antialiased h-full bg-background">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Main />
+          <NextScript />
+        </ThemeProvider>
       </body>
     </Html>
   );
